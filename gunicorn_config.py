e718172be0da91d8cac0a8036a7ad13480f5acb4
stafiobitit-beep <stafiobitit@gmail.com>
2025-11-28 13:11:58 +0100

    Add files via upload

---
+++ gunicorn_config.py
@@ -0,0 +1,34 @@
+import multiprocessing
+import os
+
+# Server socket
+bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 120
+keepalive = 5
+
+# Logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+# Process naming
+proc_name = 'odoo-excel-uploader'
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL (not needed on Render)
+keyfile = None
+certfile = None
